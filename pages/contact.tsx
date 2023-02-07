adbe68c70a6fcb11575058a9950c63576233c509
Kiah <miciahjohnhenderson@gmail.com>
2023-02-07 18:43:17 -0500

    added formspree integration

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import Chrome from '../components/Chrome';
 import FormBanner from '../components/FormBanner';
 
 const Contact: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [status, setStatus] = useState<
-    'idle' | 'success' | 'missing_field' | 'api_error'
-  >('idle');
-  const [bannerMessage, setBannerMessage] = useState('');
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM!);
+  let bannerMessage =
+    'Whoops, something went wrong on our end. Try refreshing the page and trying again.';
+  let bannerState: 'missing_field' | 'api_error' | 'success' | 'idle' = 'idle';
+  if (state.succeeded) {
+    bannerMessage =
+      "Thanks for the message! I'll try to get back to you as soon as possible.";
+    bannerState = 'success';
+  } else if (state.errors.length > 0) {
+    bannerState = 'api_error';
+    bannerMessage =
+      'Whoops, something went wrong on our end. Try refreshing the page and trying again.';
+  }
+
   return (
     <Chrome page="/contact" smallFooter>
       <div className="flex justify-center items-center py-12 px-6 sm:px-12 flex-grow">
         <div className="absolute w-full h-112 bg-sky-100 z-0"></div>
         <div className="absolute w-full h-96 bg-sky-400 z-0"></div>
-        <form className="border-[0.5px] rounded-2xl shadow-xl py-8 px-6 sm:px-8 max-w-xl bg-white relative">
+        <form
+          className="border-[0.5px] rounded-2xl shadow-xl py-8 px-6 sm:px-8 max-w-xl bg-white relative"
+          onSubmit={handleSubmit}
+        >
           <h2 className="text-2xl font-inter text-center sm:text-left">Contact me</h2>
           <p className="text-slate-500 mt-2 text-center sm:text-left">
             Feel free to contact me using the form below with any questions or comments.
@@ -22,67 +34,42 @@ const Contact: React.FC = () => {
           </p>
           <FormBanner
             message={bannerMessage}
-            type={status}
-            className={status === 'idle' ? 'my-0' : 'my-4'}
+            type={bannerState}
+            className={bannerState === 'idle' ? 'my-0' : 'my-4'}
           />
           <div className="flex flex-col">
             <div className="flex flex-col mb-8 space-y-2">
-              <label className="text-gray-600 font-medium">Email</label>
+              <label className="text-gray-600 font-medium" htmlFor="email">
+                Email
+              </label>
               <input
                 className="outline-none border py-3 px-4 rounded-lg focus:border-sky-300 transition duration-100 shadow"
                 placeholder="me@example.com"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                id="email"
+                type="email"
+                name="email"
               />
+              <ValidationError prefix="Email" field="email" errors={state.errors} />
             </div>
             <div className="flex flex-col space-y-2">
-              <label className="text-gray-600 font-medium">Message</label>
+              <label className="text-gray-600 font-medium" htmlFor="message">
+                Message
+              </label>
               <textarea
                 className="outline-none border py-3 px-4 rounded-lg focus:border-sky-300 transition duration-100 shadow"
                 rows={5}
                 placeholder="Questions or comments"
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
+                id="message"
+                name="message"
               ></textarea>
+              <ValidationError prefix="Message" field="message" errors={state.errors} />
             </div>
           </div>
           <div className="flex justify-end mt-8">
             <button
               className="flex justify-center items-center bg-sky-500 text-white rounded-lg shadow-md py-2 px-5 font-medium text-lg hover:bg-sky-400 transition duration-100 active:shadow-sm active:scale-95 active:translate-y-0.5 hover:-translate-y-0.5 flex-grow xs:flex-grow-0"
-              onClick={async (event) => {
-                event.preventDefault();
-                // if (!email) {
-                //   setStatus('missing_field');
-                //   setBannerMessage('Please fill out email field');
-                //   return;
-                // }
-                // if (!message) {
-                //   setStatus('missing_field');
-                //   setBannerMessage('Please fill out message field');
-                //   return;
-                // }
-                // const res = await fetch(`/api/send-email`, {
-                //   method: 'POST',
-                //   headers: {
-                //     'Content-Type': 'application/json',
-                //   },
-                //   body: JSON.stringify({ email, message }),
-                // });
-                // if (res.status === 200) {
-                //   setStatus('success');
-                //   setBannerMessage(
-                //     "Thanks for the message! I'll get back to you as soon as I can.",
-                //   );
-                // } else {
-                //   setStatus('api_error');
-                //   setBannerMessage(
-                //     'Whoops, looks like something went wrong on our end. Try refreshing the page and trying again.',
-                //   );
-                // }
-                alert(
-                  'Sorry, our contact form is currently not working. Check back tomorrow and it should be functional.',
-                );
-              }}
+              type="submit"
+              disabled={state.submitting}
             >
               <i className="fa-solid fa-arrow-right mr-3"></i>
               Submit
