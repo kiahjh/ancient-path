052e906cd3174eada4de502140ee381b32c16848
Kiah <miciahjohnhenderson@gmail.com>
2023-02-14 11:01:07 -0500

    fixed small bugs in PostPreview

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import striptags from 'striptags';
+import cx from 'classnames';
 import type { DualPost } from '../lib/types';
 import { englishMonths, spanishMonths } from '../lib/dates';
 import { LanguageContext } from '../lib/LanguageContext';
@@ -19,7 +20,12 @@ const PostPreview: React.FC<Props> = ({ post }) => {
     <div className="shadow-lg border-[0.5px] rounded-xl relative bg-white flex flex-col justify-start items-start md:ml-10">
       {new Date().getTime() - new Date(post.createdAt).getTime() <
         1000 * 60 * 60 * 24 * 7 && (
-        <div className="w-12 h-12 bg-sky-400 rounded-full flex justify-center items-center text-white right-4 -top-4 absolute shadow-md uppercase text-sm font-medium rotate-12">
+        <div
+          className={cx(
+            'w-12 h-12 bg-sky-400 rounded-full flex justify-center items-center text-white right-4 -top-4 absolute shadow-md uppercase font-medium rotate-12',
+            language === 'en' ? 'text-sm' : 'text-xs',
+          )}
+        >
           {language === 'en' ? 'New' : 'Nuevo'}
         </div>
       )}
@@ -32,9 +38,13 @@ const PostPreview: React.FC<Props> = ({ post }) => {
         <h3 className="text-2xl text-sky-500">{new Date(post.createdAt).getDate()}</h3>
       </div>
       <div className="p-6 md:pl-16 pt-16 md:pt-6 pb-4">
-        <div className="flex justify-start items-center space-x-4">
-          <h2 className="text-xl font-inter text-gray-900">{post[language].title}</h2>
-          {post.category === 'teaching' && <TeachingBadge language={language} />}
+        <div className="flex justify-start items-center flex-wrap">
+          <h2 className="text-xl font-inter text-gray-900 mr-3 mb-2">
+            {post[language].title}
+          </h2>
+          {post.category === 'teaching' && (
+            <TeachingBadge language={language} className="mb-2" />
+          )}
         </div>
         <p
           className="mt-3 mb-3 text-gray-500"
