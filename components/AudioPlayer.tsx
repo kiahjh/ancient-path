17c0574ae470aff83767f03df1722b9364fdbb7a
Kiah <miciahjohnhenderson@gmail.com>
2023-03-21 15:47:16 -0400

    made audio player responsive

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid';
 import cx from 'classnames';
 import styles from '../styles/AudioPlayer.module.css';
 import SoundAnimation from './SoundAnimation';
-import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid';
 
 interface Props {
   src: string;
@@ -108,8 +108,8 @@ const AudioPlayer: React.FC<Props> = ({ src, postTitle, className }) => {
           <i className="fa-solid fa-forward text-xl" />
         </button>
       </div>
-      <div className="flex items-center bg-slate-50 dark:bg-slate-700/50 p-3 space-x-8 rounded-b-xl">
-        <div className="shrink-0 flex justify-center items-center">
+      <div className="flex items-center bg-slate-50 dark:bg-slate-700/50 p-3 pb-2 sm:pb-3 sm:space-x-8 rounded-b-xl flex-wrap sm:flex-nowrap">
+        <div className="shrink-0 flex justify-start sm:justify-center items-center flex-grow sm:flex-grow-0">
           <a
             download
             href={`/api/download?url=${src}&title=${encodeURIComponent(postTitle)}`}
@@ -126,7 +126,7 @@ const AudioPlayer: React.FC<Props> = ({ src, postTitle, className }) => {
             />
           </a>
         </div>
-        <div className="flex-grow flex items-center shrink-0 space-x-2">
+        <div className="flex-grow flex items-center shrink-0 space-x-2 order-first sm:order-none w-full sm:w-auto mb-2 sm:mb-0">
           <span className="w-16 text-slate-400">{formatDuration(currentTime)}</span>
           <div className="relative flex-grow -mt-1.5">
             <input
@@ -154,7 +154,7 @@ const AudioPlayer: React.FC<Props> = ({ src, postTitle, className }) => {
             {formatDuration(duration)}
           </span>
         </div>
-        <div className="shrink-0 flex justify-center items-center">
+        <div className="shrink-0 flex justify-end sm:justify-center items-center flex-grow sm:flex-grow-0">
           <button
             className="hover:bg-slate-200/50 dark:hover:bg-slate-700 w-8 h-8 flex justify-center items-center rounded-full text-slate-400 dark:text-slate-500 dark:hover:text-slate-400  transition duration-100"
             onClick={() => {
