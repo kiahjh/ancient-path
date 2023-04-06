5d2182279ec6f5d422e7122a22544b9076aa6ac4
Kiah <miciahjohnhenderson@gmail.com>
2023-04-06 13:34:14 -0400

    added previous and next buttons to each post

--- pages/[posts]/[slug].tsx
+++ pages/[posts]/[slug].tsx
@@ -1,5 +1,7 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
 import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import cx from 'classnames';
 import AudioPlayer from '../../components/AudioPlayer';
 import PageWrapper from '../../components/PageWrapper';
 import { getAllPosts } from '../../lib/getAllPosts';
@@ -27,17 +29,30 @@ export const getStaticProps: GetStaticProps<React.ComponentProps<typeof Post>> =
 ) => {
   const allDualPosts = await getAllPosts();
   let whichLanguage: Lang = 'en';
-  const dualPost = allDualPosts.find((post) => {
+  let indexOfPost = 0;
+  const dualPost = allDualPosts.find((post, index) => {
     if (post.es.slug === context.params?.slug) {
       whichLanguage = 'es';
     }
-    return post.en.slug === context.params?.slug || post.es.slug === context.params?.slug;
+    if (post.en.slug === context.params?.slug || post.es.slug === context.params?.slug) {
+      indexOfPost = index;
+      return true;
+    }
+    return false;
   })!;
+  const nextPostSlug =
+    indexOfPost !== 0 ? allDualPosts[indexOfPost - 1][whichLanguage].slug : null;
+  const prevPostSlug =
+    indexOfPost !== allDualPosts.length - 1
+      ? allDualPosts[indexOfPost + 1][whichLanguage].slug
+      : null;
   const post = dualPost[whichLanguage];
 
   return {
     props: {
       post,
+      nextPostSlug,
+      prevPostSlug,
       alternateLanguageSlug: whichLanguage === 'en' ? dualPost.es.slug : dualPost.en.slug,
     },
   };
@@ -45,21 +60,36 @@ export const getStaticProps: GetStaticProps<React.ComponentProps<typeof Post>> =
 
 interface Props {
   post: Post<Lang>;
+  nextPostSlug: string | null;
+  prevPostSlug: string | null;
   alternateLanguageSlug: string;
 }
 
-const Post: React.FC<Props> = ({ post, alternateLanguageSlug }) => {
+const Post: React.FC<Props> = ({
+  post,
+  alternateLanguageSlug,
+  nextPostSlug,
+  prevPostSlug,
+}) => {
   const language = post.lang;
   const content = {
     en: {
       page: '/posts',
       redirectTo: `/publicaciones/${alternateLanguageSlug}`,
       title: `${post.title} | The Ancient Path`,
+      nextButtonText: 'Next post',
+      prevButtonText: 'Previous post',
+      nextButtonHref: nextPostSlug ? `/posts/${nextPostSlug}` : null,
+      prevButtonHref: prevPostSlug ? `/posts/${prevPostSlug}` : null,
     },
     es: {
       page: '/publicaciones',
       redirectTo: `/posts/${alternateLanguageSlug}`,
       title: `${post.title} | La Senda Antigua`,
+      nextButtonText: 'Siguiente publicación',
+      prevButtonText: 'Publicación anterior',
+      nextButtonHref: nextPostSlug ? `/publicaciones/${nextPostSlug}` : null,
+      prevButtonHref: prevPostSlug ? `/publicaciones/${prevPostSlug}` : null,
     },
   };
   const c = content[language];
@@ -92,6 +122,34 @@ const Post: React.FC<Props> = ({ post, alternateLanguageSlug }) => {
               dangerouslySetInnerHTML={{ __html: post.content }}
             ></div>
           </main>
+          <div className="w-full flex justify-between items-center">
+            {c.prevButtonHref ? (
+              <Link
+                href={c.prevButtonHref}
+                className={cx(
+                  `bg-sky-100 px-4 py-2 font-medium text-sky-500 rounded-lg transition duration-100 hover:bg-sky-200/70 active:scale-95`,
+                )}
+              >
+                <i className="mr-2 fa-solid fa-arrow-left" />
+                {c.prevButtonText}
+              </Link>
+            ) : (
+              <div />
+            )}
+            {c.nextButtonHref ? (
+              <Link
+                href={c.nextButtonHref}
+                className={cx(
+                  `bg-sky-100 px-4 py-2 font-medium text-sky-500 rounded-lg transition duration-100 hover:bg-sky-200/70 active:scale-95`,
+                )}
+              >
+                {c.nextButtonText}
+                <i className="ml-2 fa-solid fa-arrow-right" />
+              </Link>
+            ) : (
+              <div />
+            )}
+          </div>
         </div>
       </div>
     </PageWrapper>
