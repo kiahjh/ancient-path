7d73fbf5e2e5aaf097dc7c031d0ad6ea7b991940
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-06-05 14:09:17 -0400

    switched over to new cosmic api and dashboard

--- lib/getAllPosts.ts
+++ lib/getAllPosts.ts
@@ -5,7 +5,7 @@ const bucketSlug = process.env.COSMIC_BUCKET_SLUG;
 const readKey = process.env.COSMIC_READ_KEY;
 
 export async function getAllPosts(): Promise<DualPost[]> {
-  const endpoint = `https://api.cosmicjs.com/v2/buckets`;
+  const endpoint = `http://api.cosmicjs.com/v3/buckets`;
   const query = encodeURIComponent(`{"type":"posts"}`);
   const url = `${endpoint}/${bucketSlug}/objects?&query=${query}&read_key=${readKey}`;
   const response = await fetch(url);
