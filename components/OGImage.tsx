7faa12b5dc88f61cfa5a2d235581fba20f673e0c
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-10 10:08:08 -0400

    removed jason's picture from og image

--- components/OGImage.tsx
+++ components/OGImage.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-import ProfileImg from '../public/profile.jpg';
 import { BookOpenIcon, SpeakerWaveIcon } from '@heroicons/react/24/outline';
 import { SignalIcon } from '@heroicons/react/24/solid';
 
@@ -10,9 +8,9 @@ const OGImage: React.FC = () => {
       <div className="w-128 h-128 bg-sky-300 dark:bg-sky-400 rounded-2xl absolute left-20 -bottom-80 rotate-45 bg-opacity-20 dark:bg-opacity-5 sm:dark:bg-opacity-10 sm:bg-opacity-50"></div>
       <div className="w-96 h-96 bg-sky-500 rounded-2xl absolute -left-8 -bottom-64 rotate-45 bg-opacity-20 dark:bg-opacity-5 sm:dark:bg-opacity-10 sm:bg-opacity-50"></div>
       <h1 className="font-inter text-8xl p-16 relative leading-[105px]">
-        <span className="text-slate-700">La Senda</span>
+        <span className="text-slate-700">The</span>
         <br />
-        <span className="text-sky-500">Antigua</span>
+        <span className="text-sky-500">Ancient Path</span>
       </h1>
       <div className="flex flex-row items-center gap-6 py-4 px-4 bg-white/70 backdrop-blur-xl rounded-full absolute left-8 bottom-8 w-fit shadow border border-sky-200">
         <div className="flex justify-center items-center gap-2 bg-white/50 border border-sky-200 shadow-inner w-20 h-20 rounded-full">
@@ -25,12 +23,7 @@ const OGImage: React.FC = () => {
           <SignalIcon className="h-12 text-sky-500" />
         </div>
       </div>
-      <Image
-        src={ProfileImg}
-        alt={'Jason R. Henderson'}
-        className="w-96 h-128 object-cover rounded-tl-[80px] absolute right-0 bottom-0"
-      />
-      <span className="absolute bottom-8 right-[424px] text-3xl font-bold text-slate-700">
+      <span className="absolute bottom-12 right-12 text-5xl font-bold text-slate-700">
         Jason R. Henderson
       </span>
     </div>
