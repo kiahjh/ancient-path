cd449a3ab597c55e6e0ccfabcd000545562f8319
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-11 16:36:19 -0400

    made basic homepage

---
+++ app/books/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Books: React.FC = () => (
+  <div className="p-20 flex justify-center items-center h-full text-3xl text-slate-800 font-bold">
+    Coming soon: free book downloads
+  </div>
+);
+
+export default Books;
