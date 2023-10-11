a7e032e0a8b3d512742ed4cdaf34479e7f06db82
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-11 16:36:19 -0400

    made basic homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,54 +22,56 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <html lang="TODO">
       <body
         style={{ background: `#e0f2fe url(${NoiseBg.src})` }}
-        className={cx(`flex min-h-screen bg-cover bg-center`, roboto)}
+        className={cx(`flex flex-col min-h-screen bg-cover bg-center`, roboto)}
       >
-        <div className="w-72 flex flex-col justify-between">
-          <GlobalNav />
-          <div>
-            <div
-              className={cx(
-                `mx-4 rounded-3xl flex flex-col transition-[background-color,height,padding] duration-300`,
-                settingsPanelOpen ? `h-60 bg-sky-300/30 p-2` : `h-12`,
-              )}
-            >
-              <button
-                onClick={() => setSettingsPanelOpen(!settingsPanelOpen)}
+        <div className="flex-grow flex">
+          <div className="w-72 flex flex-col justify-between shrink-0">
+            <GlobalNav />
+            <div>
+              <div
                 className={cx(
-                  `flex justify-between items-center h-12 transition-[background-color,padding] duration-300 hover:bg-sky-300/20 rounded-2xl`,
-                  settingsPanelOpen ? `px-4` : `px-6`,
+                  `mx-4 rounded-3xl flex flex-col transition-[background-color,height,padding] duration-300`,
+                  settingsPanelOpen ? `h-60 bg-sky-300/30 p-2` : `h-12`,
                 )}
               >
-                <div className="flex items-center gap-2">
-                  <Cog6ToothIcon className="w-6 h-6 text-sky-500" />
-                  <span className="font-medium text-sky-800/80">Settings</span>
-                </div>
-                <PlusIcon
+                <button
+                  onClick={() => setSettingsPanelOpen(!settingsPanelOpen)}
                   className={cx(
-                    `w-6 h-6 text-sky-500 transition-transform duration-300`,
-                    settingsPanelOpen && `rotate-45`,
+                    `flex justify-between items-center h-12 transition-[background-color,padding,border-radius] duration-300 hover:bg-sky-300/20 active:bg-sky-300/40`,
+                    settingsPanelOpen ? `px-4 rounded-2xl` : `px-6 rounded-3xl`,
                   )}
-                />
-              </button>
-            </div>
-            <div className="h-24 flex items-center justify-center gap-4">
-              <button className="w-14 h-10 rounded-2xl hover:bg-sky-200/60 flex justify-center items-center transition-[background-color,transform] duration-200 active:bg-sky-300/70 active:scale-95">
-                <BackwardIcon className="w-8 ml-0.5 text-sky-500" />
-              </button>
-              <button className="w-12 h-12 rounded-full bg-sky-500 hover:bg-sky-600 active:bg-sky-700 active:scale-95 transition-[background-color,transform] duration-200 flex justify-center items-center">
-                <PlayIcon className="w-6 ml-0.5 text-white" />
-              </button>
-              <button className="w-14 h-10 rounded-2xl hover:bg-sky-200/60 flex justify-center items-center transition-[background-color,transform] duration-200 active:bg-sky-300/70 active:scale-95">
-                <ForwardIcon className="w-8 ml-0.5 text-sky-500" />
-              </button>
+                >
+                  <div className="flex items-center gap-2">
+                    <Cog6ToothIcon className="w-6 h-6 text-sky-500" />
+                    <span className="font-medium text-sky-800/80">
+                      Settings
+                    </span>
+                  </div>
+                  <PlusIcon
+                    className={cx(
+                      `w-6 h-6 text-sky-500 transition-transform duration-300`,
+                      settingsPanelOpen && `rotate-45`,
+                    )}
+                  />
+                </button>
+              </div>
             </div>
           </div>
+          <main className="flex-grow rounded-bl-3xl bg-sky-50">{children}</main>
         </div>
-        <div className="flex flex-col min-h-screen flex-grow">
-          <main className="flex-grow rounded-bl-3xl bg-sky-50 p-20">
-            {children}
-          </main>
-          <div className="h-24 flex items-center justify-end pr-8 gap-8">
+        <div className="h-24 flex items-center justify-between gap-8 pr-8">
+          <div className="h-24 flex items-center justify-center gap-4 w-72">
+            <button className="w-14 h-10 rounded-2xl hover:bg-sky-200/60 flex justify-center items-center transition-[background-color,transform] duration-200 active:bg-sky-300/70 active:scale-95">
+              <BackwardIcon className="w-8 ml-0.5 text-sky-500" />
+            </button>
+            <button className="w-12 h-12 rounded-full bg-sky-500 hover:bg-sky-600 active:bg-sky-700 active:scale-95 transition-[background-color,transform] duration-200 flex justify-center items-center">
+              <PlayIcon className="w-6 ml-0.5 text-white" />
+            </button>
+            <button className="w-14 h-10 rounded-2xl hover:bg-sky-200/60 flex justify-center items-center transition-[background-color,transform] duration-200 active:bg-sky-300/70 active:scale-95">
+              <ForwardIcon className="w-8 ml-0.5 text-sky-500" />
+            </button>
+          </div>
+          <div className="flex justify-center items-center gap-8">
             <Link
               href="/about"
               className="bg-sky-50 w-12 h-12 rounded-full flex justify-center items-center text-sky-600/60 hover:bg-white transition-colors duration-200"
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 
 const Home: React.FC = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="p-28 flex flex-col items-center justify-between h-full">
+    <div className="flex flex-col items-center">
+      <h1 className="font-black text-6xl text-slate-800">The Ancient Path</h1>
+      <h2 className="mt-6 text-xl max-w-2xl text-center text-slate-800/70">
+        A place to share my thoughts and experiences about the way God works in
+        the soul of man.
+      </h2>
+    </div>
+    <div className="p-8 border border-dashed border-sky-500/40 rounded-3xl flex flex-col items-center relative">
+      <span className="text-lg text-sky-500/70 absolute -top-4 bg-sky-50 px-4">
+        Recent posts
+      </span>
+      <div className="flex gap-8 flex-wrap justify-center">
+        <div className="w-96 bg-white p-8 rounded-3xl">
+          <h4 className="text-sky-500/70">3 days ago</h4>
+          <h3 className="text-xl font-bold text-slate-800">The Work of God</h3>
+          <p className="text-slate-500 mt-2">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+        </div>
+        <div className="w-96 bg-white p-8 rounded-3xl">
+          <h4 className="text-sky-500/70">last week</h4>
+          <h3 className="text-xl font-bold text-slate-800">The Work of God</h3>
+          <p className="text-slate-500 mt-2">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+        </div>
+        <div className="w-96 bg-white p-8 rounded-3xl">
+          <h4 className="text-sky-500/70">2 weeks ago</h4>
+          <h3 className="text-xl font-bold text-slate-800">The Work of God</h3>
+          <p className="text-slate-500 mt-2">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
