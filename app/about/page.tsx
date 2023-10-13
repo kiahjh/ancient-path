f6c13030e94fa8e75589652b994597745f47ed73
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-13 16:04:25 -0400

    added ability to read teachings

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About: React.FC = () => (
+  <div className="p-20 flex justify-center items-center h-full text-3xl text-slate-800 font-bold">
+    Coming soon
+  </div>
+);
+
+export default About;
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import type { Post } from "@/lib/types";
 
 interface Props {
@@ -6,10 +7,15 @@ interface Props {
 }
 
 const PostPreview: React.FC<Props> = ({ post }) => (
-  <div className="bg-white p-8 rounded-3xl">
+  <Link
+    href={`/${post.category === `teaching` ? `teachings` : `posts`}/${
+      post.en.slug
+    }`}
+    className="bg-white p-8 rounded-3xl"
+  >
     <h3 className="text-xl font-bold text-slate-800">{post.en.title}</h3>
     <p className="mt-2 text-slate-800/80">{post.en.description}</p>
-  </div>
+  </Link>
 );
 
 export default PostPreview;
