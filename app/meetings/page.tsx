bd226c1903e78aaf79356e646e9e4441c609e32c
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-03-18 14:16:24 -0400

    placeholder for meetings page

---
+++ app/meetings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Meetings: React.FC = () => {
+  return (
+    <div className="min-h-full flex justify-center items-center text-2xl font-medium">
+      Coming soon
+    </div>
+  );
+};
+
+export default Meetings;
---
+++ app/reuniones/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reuniones: React.FC = () => {
+  return (
+    <div className="min-h-full flex justify-center items-center text-2xl font-medium">
+      Próximamente
+    </div>
+  );
+};
+
+export default Reuniones;
