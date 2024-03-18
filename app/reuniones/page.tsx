993e6f3929c16e18975ad961c6ead3915913d4ba
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-03-18 14:54:01 -0400

    added seo metadata to new pages

--- app/reuniones/page.tsx
+++ app/reuniones/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+export const metadata = {
+  title: `Reuniones | La Senda Antigua`,
+  description: `Grabaciones de audio de algunas reuniones.`,
+  openGraph: {
+    title: `Reuniones | La Senda Antigua`,
+    description: `Grabaciones de audio de algunas reuniones.`,
+  },
+};
+
 const Reuniones: React.FC = () => {
   return (
     <div className="min-h-full flex justify-center items-center text-2xl font-medium">
