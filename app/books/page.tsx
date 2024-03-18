e2bc44886da62f46e67792618e08f605d3970e8c
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-03-18 14:54:01 -0400

    added seo metadata to new pages

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import type { NextPage } from "next";
 import BooksPageTemplate from "@/components/templates/BooksPageTemplate";
 
+export const metadata = {
+  title: "Books | The Ancient Path",
+  description: `Books from other writers and authers that I highly recommend.`,
+  openGraph: {
+    title: "Books | The Ancient Path",
+    description: `Books from other writers and authers that I highly recommend.`,
+  },
+};
+
 const Books: NextPage = () => <BooksPageTemplate language="en" />;
 
 export default Books;
--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+export const metadata = {
+  title: `Meetings | The Ancient Path`,
+  description: `Audio recordings from some meetings.`,
+  openGraph: {
+    title: `Meetings | The Ancient Path`,
+    description: `Audio recordings from some meetings.`,
+  },
+};
+
 const Meetings: React.FC = () => {
   return (
     <div className="min-h-full flex justify-center items-center text-2xl font-medium">
